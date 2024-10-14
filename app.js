239eb0cd296641024d13508dff388130c2174784
Cryptocryxx <143177997+Cryptocryxx@users.noreply.github.com>
2024-10-14 21:24:20 +0200

    test

--- app.js
+++ app.js
@@ -18,3 +18,5 @@ app.use("/register", registerRouter)
 app.use("/static", express.static(__dirname + '/static'))
 
 app.listen(PORT, () => console.log('Server is listening on PORT 3000...'))
+
+//test commit
