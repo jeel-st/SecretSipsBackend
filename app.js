4ad2eb8f23a824e14dbd3d7b8d13658450d53cd9
Joel Starkov <js486@hdm-stuttgart.de>
2024-10-14 19:42:19 +0200

    port nummer

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-const PORT = 3000
+const PORT = 3003
 const { connectToDB } = require("./databases/databaseMain")
 
 connectToDB()
@@ -17,4 +17,4 @@ app.use("/register", registerRouter)
 
 app.use("/static", express.static(__dirname + '/static'))
 
-app.listen(PORT, () => console.log('Server is listening on PORT 3000...'))
+app.listen(PORT, () => console.log('Server is listening on PORT 3003...'))
