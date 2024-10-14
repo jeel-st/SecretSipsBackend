487205e0c805156aa4d6e3733250f2c01ec0d2a7
Joel Starkov <js486@hdm-stuttgart.de>
2024-10-14 21:45:25 +0200

    debug

--- controllers/usernameController.js
+++ controllers/usernameController.js
@@ -3,7 +3,13 @@ const database = require("../databases/databaseUsername")
 async function createUsername(req, res){
     try{
         const result = await database.createUsername(req, res)
-        res.send("Success!")
+        if(result=="Username is required"){
+            res.status(400).send("Username is required")
+        }else if(result == "User already exists"){
+            res.status(409).send("User already exists")
+        }else{
+            res.send("Success!")
+        }
     }catch(err){
         res.status(500).send("Something went wrong.")
     }
--- databases/databaseUsername.js
+++ databases/databaseUsername.js
@@ -5,13 +5,12 @@ async function createUsername(req, res){
     const {username} = req.body
     console.log("username"+ username)
     if(!username){
-        return res.status(400).send("Username is required")
+        return "Username is required"
     }
     const usernameDatabase = await database.checkUserExists(username)== true
     console.log("usernameDatabase"+ usernameDatabase)
     if(usernameDatabase){
-
-        return res.send("User already exists")
+        return "User already exists"
     }
     try{
         const result = await database.getDB().collection("users").insertOne({name: username})
