458c8ce456c0461f8b686fdc6a0053d9c3f246b9
Joel Starkov <js486@hdm-stuttgart.de>
2024-10-14 23:34:45 +0200

    x

--- databases/databaseMissions.js
+++ databases/databaseMissions.js
@@ -10,7 +10,7 @@ async function getAllMissions() {
 }
 
 async function getNotPersonalisedMissions() {
-    const nonPersonalMissions = await database.getDB().missions.find({personal: "false"})
+    const nonPersonalMissions = await database.getDB().collection("missions").find({personal: "false"})
     console.log("nonPersonalMissions"+ nonPersonalMissions)
             if (nonPersonalMissions.length === 0) {
                 throw new Error("Keine nicht-personalisierten Missionen gefunden.");
