30827362dab62a3f2449c1b410494f8bed1b721d
jstarkov <js486@hdm-stuttgart.de>
2024-10-16 00:06:44 +0200

    debug

--- routes/missionsRouter.js
+++ routes/missionsRouter.js
@@ -2,6 +2,6 @@ const express = require("express")
 const router = express.Router()
 const missionController = require("../controllers/missionsController")
 
-router.put("/missionPassed", missionController)
+router.put("/missionPassed", missionController.missionPassed)
 
 module.exports = router
