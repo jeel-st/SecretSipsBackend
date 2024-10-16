08add1f5636d02a0f7926ae22427d339fea90c93
Joel Starkov <js486@hdm-stuttgart.de>
2024-10-16 19:39:14 +0200

    logs

--- controllers/missionsController.js
+++ controllers/missionsController.js
@@ -18,9 +18,12 @@ async function missionPassed(req, res){
         const allMissions = await database.getAllMissions()
         const passedMissions = updateUser.missionPassed || []
         const failedMissions = updateUser.failedMissions || []
+        console.log(`passedMissions: ${passedMissions}`)
+        console.log(`failedMissions: ${failedMissions}`)
         const availableMissions = allMissions.filter(
-            mission => !passedMissions.includes(mission.id) || !failedMissions.includes(mission.id)
+            mission => !passedMissions.includes(mission.id) && !failedMissions.includes(mission.id)
         );
+        console.log(`availableMissions: ${availableMissions}`)
         if(availableMissions.length === 0){
             return res.status(400).json({ message: "No new missions available" });
 
@@ -56,7 +59,7 @@ async function missionFailed(req, res){
         const passedMissions = updateUser.missionPassed || []
         const failedMissions = updateUser.missionFailed || []
         const availableMissions = allMissions.filter(
-            mission => !passedMissions.includes(mission.id) || !failedMissions.includes(mission.id)
+            mission => !passedMissions.includes(mission.id) && !failedMissions.includes(mission.id)
         );
 
         if(availableMissions.length === 0){
